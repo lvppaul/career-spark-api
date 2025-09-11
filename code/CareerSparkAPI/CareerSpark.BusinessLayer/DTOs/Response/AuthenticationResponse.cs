fbbbb09f36667ff82874f544d4e0efb29bbfd8f0
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-09-12 02:28:23 +0700

    chore(BE): Fix Response Token

--- code/CareerSparkAPI/CareerSpark.BusinessLayer/DTOs/Response/AuthenticationResponse.cs
+++ code/CareerSparkAPI/CareerSpark.BusinessLayer/DTOs/Response/AuthenticationResponse.cs
@@ -1,12 +1,16 @@
-﻿namespace CareerSpark.BusinessLayer.DTOs.Response
+﻿using System.Text.Json.Serialization;
+
+namespace CareerSpark.BusinessLayer.DTOs.Response
 {
     public class AuthenticationResponse
     {
         public bool Success { get; set; }
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
-        public string? Message { get; set; }
 
+        public string? Message { get; set; }
+        // Trong trường họp null thì không serialize field này
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public AuthenticationData? Data { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public List<string>? Errors { get; set; }
     }
 }
