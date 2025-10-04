a089b4fe8b45d884071646376bd3a50c7a8c4104
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-10-05 03:03:32 +0700

    chore(BE): Change Response API
    Change launchSettings and Add ApiResponse

---
+++ code/CareerSparkAPI/CareerSpark.API/Responses/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CareerSpark.API.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Payload { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
