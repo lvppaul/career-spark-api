6ee294fb6a973a1fd8ffe6f20d82ed57fd6e325e
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-10-15 01:24:40 +0700

    feat(BE): Add Confirm Email Service

---
+++ code/CareerSparkAPI/CareerSpark.BusinessLayer/DTOs/Request/ConfirmEmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CareerSpark.BusinessLayer.DTOs.Request
+{
+
+    public class ConfirmEmailRequest
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Token { get; set; } = null!;
+
+    }
+
+}
